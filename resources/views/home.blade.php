d5500d84ce1ad47449d646330f4a25ac15384ae6
Toni Ginard <toni.ginard@gmail.com>
2023-04-26 14:56:18 +0200

    Initial home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    Contingut principal
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static function () {
+    return view('home');
+})->name('home');
+
+Route::resource('/services', ServiceController::class);
