c11b31626a250c1e711dd424b242bf25cc387c7b
Toni Ginard <toni.ginard@gmail.com>
2023-04-27 11:58:06 +0200

    ClientType: Added model

---
+++ app/Http/Controllers/ClientTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreClientTypeRequest;
+use App\Http\Requests\UpdateClientTypeRequest;
+use App\Models\ClientType;
+
+class ClientTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClientTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClientType $clientType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClientType $clientType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClientTypeRequest $request, ClientType $clientType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClientType $clientType)
+    {
+        //
+    }
+}
---
+++ database/seeders/ClientTypeSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class ClientTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $client_types = [
+            1 => 'Escola',
+            2 => 'Institut',
+            3 => 'Institut-Escola',
+            4 => 'Formació d\'adults',
+            5 => 'Servei educatiu',
+            6 => 'Escola Oficial d\'Idiomes',
+            7 => 'Altres',
+            8 => 'CEE',
+            9 => 'Centre concertat',
+            10 => 'ECA',
+            11 => 'ZER',
+            12 => 'Projecte',
+        ];
+
+        foreach ($client_types as $id => $name) {
+            DB::table('client_types')->insert([
+                'id' => $id,
+                'name' => $name,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
