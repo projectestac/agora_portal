dc8004b1439f84df98edb562c70299de2602100b
Toni Ginard <toni.ginard@gmail.com>
2023-04-27 16:29:41 +0200

    ModelType: Added model

---
+++ app/Http/Controllers/ModelTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreModelTypeRequest;
+use App\Http\Requests\UpdateModelTypeRequest;
+use App\Models\ModelType;
+
+class ModelTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreModelTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ModelType $modelType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ModelType $modelType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateModelTypeRequest $request, ModelType $modelType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ModelType $modelType)
+    {
+        //
+    }
+}
