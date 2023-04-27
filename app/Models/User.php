1c23e2342f6452d4c2cdcb58277bdf9be6606f71
Toni Ginard <toni.ginard@gmail.com>
2023-04-27 15:03:31 +0200

    User model: Added seeder and table relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -41,4 +42,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function request(): belongsTo {
+        return $this->belongsTo(Request::class);
+    }
+
+    public function manager(): belongsTo {
+        return $this->belongsTo(Manager::class);
+    }
+
 }
