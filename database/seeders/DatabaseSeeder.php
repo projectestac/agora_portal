b403152f2f13475fa621efeb87c6b9f8ff5740a0
Toni Ginard <toni.ginard@gmail.com>
2023-04-27 16:44:05 +0200

    General seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            ClientTypeSeeder::class,
+            LocationSeeder::class,
+            ClientSeeder::class,
+            ServiceSeeder::class,
+            ManagerSeeder::class,
+            ModelTypeSeeder::class,
+            ClientServiceSeeder::class,
+            CommandSeeder::class,
+            RequestTypeSeeder::class,
+            RequestTypeServiceSeeder::class,
+            RequestSeeder::class,
+        ]);
     }
 }
