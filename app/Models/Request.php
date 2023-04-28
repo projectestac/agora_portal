a01afde56e7406282fb847682601ab79f208ecf3
Toni Ginard <toni.ginard@gmail.com>
2023-04-28 13:43:44 +0200

    Request: Added model

---
+++ app/Models/Request.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Request extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'request_type_id',
+        'service_id',
+        'client_id',
+        'user_id',
+        'status',
+        'user_comment',
+        'admin_comment',
+        'private_note',
+    ];
+
+    public function requestType(): BelongsTo {
+        return $this->belongsTo(RequestType::class);
+    }
+
+    public function service(): BelongsTo {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function client(): BelongsTo {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+}
