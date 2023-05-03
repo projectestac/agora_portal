4d0d7689154f491e16c85c71ed7dcf1c1d5ecb50
Toni Ginard <toni.ginard@gmail.com>
2023-05-03 20:19:54 +0200

    Services: First steps

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -5,62 +5,58 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreServiceRequest;
 use App\Http\Requests\UpdateServiceRequest;
 use App\Models\Service;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 
-class ServiceController extends Controller
-{
+class ServiceController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index(): View|Application|Factory|ApplicationContract {
+        return view('service.index')->with('services', Service::all());
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreServiceRequest $request)
-    {
+    public function store(StoreServiceRequest $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Service $service)
-    {
+    public function show(Service $service) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Service $service)
-    {
+    public function edit(Service $service) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateServiceRequest $request, Service $service)
-    {
+    public function update(UpdateServiceRequest $request, Service $service) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Service $service)
-    {
+    public function destroy(Service $service) {
         //
     }
 }
---
+++ lang/ca/service.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'name' => 'Nom',
+    'description' => 'Descripció',
+    'url' => 'URL',
+    'quota' => 'Quota',
+    'actions' => 'Accions',
+    'show' => 'Mostra',
+    'edit' => 'Edita',
+    'delete' => 'Esborra',
+    'service' => 'Servei',
+    'services' => 'Serveis',
+    'service_list' => 'Llista de serveis',
+    'instance' => 'Instància',
+    'instances' => 'Instàncies',
+    'instance_list' => 'Llista d\'instàncies',
+    'no_services' => 'No hi ha serveis',
+];
