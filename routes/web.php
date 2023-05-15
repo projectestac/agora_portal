0d8787b84fb28c19f1437ffe89434decca801ea8
Toni Ginard <toni.ginard@gmail.com>
2023-05-15 16:13:15 +0200

    Breeze: Added default install

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,15 @@ Route::get('/', static function () {
 })->name('home');
 
 Route::resource('/services', ServiceController::class);
+
+Route::get('/dashboard', static function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
