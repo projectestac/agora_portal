f7d099967a21f0008464c404d35488a031293e59
Toni Ginard <toni.ginard@gmail.com>
2023-05-15 17:40:43 +0200

    Breeze: Removed functionality that won't be used

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,4 @@ Route::get('/', static function () {
 
 Route::resource('/services', ServiceController::class);
 
-Route::get('/dashboard', static function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
