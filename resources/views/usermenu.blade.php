a6022bf476b04d143207faff5a619bcd810275a4
Toni Ginard <toni.ginard@gmail.com>
2023-05-22 12:24:26 +0200

    Customized login page and improved site header

--- resources/views/usermenu.blade.php
+++ resources/views/usermenu.blade.php
@@ -8,12 +8,21 @@
         </button>
         <span class="navbar-brand visible-xs"></span>
     </div>
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse navbar-ex6-collapse">
         <ul class="nav navbar-nav">
-            <li><a href="#">Entra</a></li>
+            <li><a href="{{ route('login') }}">Entra</a></li>
             <li><a href="#">PMF</a></li>
             <li><a href="#">El meu Àgora</a></li>
-            <li><a href="#">Surt</a></li>
+            <li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-dropdown-link :href="route('logout')"
+                                     onclick="event.preventDefault(); this.closest('form').submit();">
+                        Surt
+                    </x-dropdown-link>
+                </form>
+
+            </li>
         </ul>
     </div>
 </nav>
