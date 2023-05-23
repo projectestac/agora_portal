4bf803d816db6ebe0ea129ecbff447282c53258e
Toni Ginard <toni.ginard@gmail.com>
2023-05-23 10:16:18 +0200

    Requests: First steps

---
+++ lang/ca/request.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'request' => 'Sol·licitud',
+    'requests' => 'Sol·licituds',
+    'user_comment' => 'Comentaris de l\'usuari',
+    'admin_comment' => 'Comentaris de l\'administrador',
+    'private_note' => 'Notes privades',
+    'request_list' => 'Llista de sol·licituds',
+    'no_requests' => 'No hi ha sol·licituds',
+];
