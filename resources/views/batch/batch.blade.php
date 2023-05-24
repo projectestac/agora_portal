074ef62d8d3f6a17acd9382a805e986053db6145
Toni Ginard <toni.ginard@gmail.com>
2023-05-24 10:03:58 +0200

    Batch: Added controller

---
+++ resources/views/batch/batch.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.default')
+
+@section('content')
+    <div class="admin-menu-container">
+        @include('menu.adminmenu')
+    </div>
+
+    <div class="content batch">
+    </div>
+@endsection
