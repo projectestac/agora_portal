eec8db0859855c7ab6dba40146b8848038c130d0
Toni Ginard <toni.ginard@gmail.com>
2023-05-25 18:29:49 +0200

    MyAgora: Added controller

---
+++ lang/ca/myagora.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'no_instances' => 'No s\'ha trobat cap servei',
+    'instance_list' => 'Llista de serveis del centre :name',
+    'no_client_access' => 'Per accedir a aquesta pàgina has de ser gestor/a d\'algun centre.',
+    'active_date' => 'Data d\'alta',
+    'requested_by' => 'Ha fet la sol·licitud',
+    'database' => 'Base de dades',
+];
