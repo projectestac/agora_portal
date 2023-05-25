b777cf30fa579e2795a088da01ca42b90447d957
Toni Ginard <toni.ginard@gmail.com>
2023-05-25 18:31:42 +0200

    Added client menu and improved admin menu

---
+++ resources/views/menu/clientmenu.blade.php
@@ -0,0 +1,35 @@
+@canany(['Administrate site', 'Manage own managers', 'Manage clients'])
+    <nav class="navbar-default admin-menu" role="navigation">
+        <ul class="nav navbar-nav primary">
+            @canany(['Administrate site', 'Manage clients'])
+                <li>
+                    <a href="{{ route('myagora.instances') }}" @if (str_contains(request()->url(), 'instance')) class="selected" @endif>
+                        {{ __('service.services') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('myagora.files') }}" @if (str_contains(request()->url(), 'files')) class="selected" @endif>
+                        Gestió de fitxers
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('myagora.requests') }}" @if (str_contains(request()->url(), 'requests')) class="selected" @endif>
+                        {{ __('request.requests') }}
+                    </a>
+                </li>
+            @endcanany
+            <li>
+                <a href="{{ route('myagora.managers') }}" @if (str_contains(request()->url(), 'managers')) class="selected" @endif>
+                    Gestors
+                </a>
+            </li>
+            @canany(['Administrate site', 'Manage clients'])
+                <li>
+                    <a href="{{ route('myagora.logs') }}" @if (str_contains(request()->url(), 'logs')) class="selected" @endif>
+                        Registre d'accions
+                    </a>
+                </li>
+            @endcanany
+        </ul>
+    </nav>
+@endcan
