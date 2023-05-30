7335f8970a051e029b37cb5f0ad564dda8f65382
Toni Ginard <toni.ginard@gmail.com>
2023-05-30 13:15:24 +0200

    Manager: Completed managers section in myagora

--- app/Http/Requests/StoreManagerRequest.php
+++ app/Http/Requests/StoreManagerRequest.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Requests;
 
+use App\Helpers\Access;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreManagerRequest extends FormRequest
-{
+class StoreManagerRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return Access::isAdmin($this->user()) ||
+            Access::isClient($this->user()) ||
+            Access::isManager($this->user());
     }
 
     /**
@@ -19,10 +20,23 @@ class StoreManagerRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            'username' => [
+                'required',
+                'string',
+                'max:17',
+                'regex:/^(?:[a-zA-Z0-9]{1,8}|[a-zA-Z0-9]{1,8}@xtec\.cat)$/',
+            ],
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'username.required' => __('manager.username_required'),
+            'username.string' => __('manager.username_string'),
+            'username.max' => __('manager.username_max'),
+            'username.regex' => __('manager.username_regex'),
         ];
     }
 }
