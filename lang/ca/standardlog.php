7eedf34d2efdc967c18433e1b48e4b2c45c0fbca
Toni Ginard <toni.ginard@gmail.com>
2023-05-30 18:11:48 +0200

    Log: Added myagora functionality

---
+++ lang/ca/standardlog.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'action_type' => 'Tipus d\'acció',
+    'action_description' => 'Descripció',
+    'delete' => 'Esborra :name com a gestor',
+    'action_add' => 'Addició / Alta',
+    'action_edit' => 'Modificació / Notificació',
+    'action_delete' => 'Eliminació / Baixa',
+    'action_error' => 'Error',
+    'action_admin' => 'Administració',
+];
