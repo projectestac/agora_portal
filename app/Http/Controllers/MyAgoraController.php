d51520c0a1b0e17aa3325e82be9c91af56e741fc
Toni Ginard <toni.ginard@gmail.com>
2023-05-30 13:52:37 +0200

    MyAgora: Improved sections of managers and instances

--- app/Http/Controllers/MyAgoraController.php
+++ app/Http/Controllers/MyAgoraController.php
@@ -10,36 +10,26 @@ use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MyAgoraController extends Controller {
-    public function myagora(): View|Application|Factory|ApplicationContract {
-        return view('myagora.index');
+
+    public const MAX_MANAGERS = 4;
+
+    public function myagora(): RedirectResponse{
+        return redirect()->route('myagora.instances');
     }
 
     public function instances(Request $request): View|Application|Factory|ApplicationContract {
-
-        if (Access::isClient(Auth::user())) {
-            return view('myagora.no_access')->with('message', __('myagora.no_client_access'));
-        }
-
-        // Get the clients from the session or the database. In case the clients are not in the
-        // session, get them from the database and store them in the session.
-        $clients = Cache::get_clients($request);
 
         // If the current user is admin, try to get a client code from the URL. Otherwise, get the
         // current client from the session.
         if (Access::isAdmin(Auth::user())) {
             $current_client = Util::get_client_from_url($request);
         } else {
-            $current_client = $request->session()->get('current_client');
-        }
-
-        // If there is no current client, get the first client from the list of clients if there is any.
-        if (is_null($current_client) && count($clients) > 0) {
-            $current_client = $clients[0];
-            $request->session()->put('current_client', $current_client);
+            $current_client = Cache::get_current_client($request);
         }
 
         if (empty($current_client)) {
@@ -53,15 +43,42 @@ class MyAgoraController extends Controller {
     }
 
     public function files(): View|Application|Factory|ApplicationContract {
+        if (Access::isClient(Auth::user())) {
+            return view('myagora.no_access')->with('message', __('myagora.no_client_access'));
+        }
+
         return view('myagora.file');
     }
 
     public function requests(): View|Application|Factory|ApplicationContract {
+        if (Access::isClient(Auth::user())) {
+            return view('myagora.no_access')->with('message', __('myagora.no_client_access'));
+        }
+
         return view('myagora.request');
     }
 
-    public function managers(): View|Application|Factory|ApplicationContract {
-        return view('myagora.manager');
+    public function managers(Request $request): View|Application|Factory|ApplicationContract {
+
+        if (Access::isAdmin(Auth::user())) {
+            $current_client = Util::get_client_from_url($request);
+        } else {
+            $current_client = Cache::get_current_client($request);
+        }
+
+        // Get an array of objects of type Manager.
+        $client = Client::find($current_client['id']);
+        $managers = $client->managers->all();
+
+        foreach ($managers as $manager) {
+            $manager->name = $manager->user->name;
+            $manager->email = $manager->user->email;
+        }
+
+        return view('myagora.manager')
+            ->with('managers', $managers)
+            ->with('current_client', $current_client)
+            ->with('max_managers', self::MAX_MANAGERS);
     }
 
     public function logs(): View|Application|Factory|ApplicationContract {
