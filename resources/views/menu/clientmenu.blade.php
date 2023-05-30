d0fe7dc8e847cec4c4fd09d4ce0f440ab69dddc5
Toni Ginard <toni.ginard@gmail.com>
2023-05-30 18:43:04 +0200

    Updated client menu and user menu

--- resources/views/menu/clientmenu.blade.php
+++ resources/views/menu/clientmenu.blade.php
@@ -1,35 +1,33 @@
 @canany(['Administrate site', 'Manage own managers', 'Manage clients'])
     <nav class="navbar-default admin-menu" role="navigation">
         <ul class="nav navbar-nav primary">
-            @canany(['Administrate site', 'Manage clients'])
-                <li>
-                    <a href="{{ route('myagora.instances') }}" @if (str_contains(request()->url(), 'instance')) class="selected" @endif>
-                        {{ __('service.services') }}
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('myagora.files') }}" @if (str_contains(request()->url(), 'files')) class="selected" @endif>
-                        Gestió de fitxers
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('myagora.requests') }}" @if (str_contains(request()->url(), 'requests')) class="selected" @endif>
-                        {{ __('request.requests') }}
-                    </a>
-                </li>
-            @endcanany
             <li>
-                <a href="{{ route('myagora.managers') }}" @if (str_contains(request()->url(), 'managers')) class="selected" @endif>
-                    Gestors
+                <a href="{{ route('myagora.instances') }}" @if (str_contains(request()->url(), 'instance')) class="selected" @endif>
+                    {{ __('service.services') }}
                 </a>
             </li>
             @canany(['Administrate site', 'Manage clients'])
                 <li>
-                    <a href="{{ route('myagora.logs') }}" @if (str_contains(request()->url(), 'logs')) class="selected" @endif>
-                        Registre d'accions
+                    <a href="{{ route('myagora.files') }}" @if (str_contains(request()->url(), 'files')) class="selected" @endif>
+                        {{ __('file.files') }}
                     </a>
                 </li>
             @endcanany
+            <li>
+                <a href="{{ route('myagora.requests') }}" @if (str_contains(request()->url(), 'requests')) class="selected" @endif>
+                    {{ __('request.requests') }}
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('myagora.managers') }}" @if (str_contains(request()->url(), 'managers')) class="selected" @endif>
+                    {{ __('manager.managers') }}
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('myagora.logs') }}" @if (str_contains(request()->url(), 'logs')) class="selected" @endif>
+                    {{ __('standardlog.title') }}
+                </a>
+            </li>
         </ul>
     </nav>
 @endcan
