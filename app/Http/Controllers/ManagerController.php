77fae120039e4b7adc4d8556ae6d20aa338b3157
Toni Ginard <toni.ginard@gmail.com>
2023-05-30 13:15:24 +0200

    Manager: Completed managers section in myagora

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -2,65 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Cache;
 use App\Http\Requests\StoreManagerRequest;
 use App\Http\Requests\UpdateManagerRequest;
 use App\Models\Manager;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
 
-class ManagerController extends Controller
-{
+class ManagerController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
         //
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreManagerRequest $request)
-    {
-        //
+    public function store(StoreManagerRequest $request): RedirectResponse {
+
+        $username = mb_strtolower($request->get('username'));
+
+        // $username must have the form aginard, and $email must have the form aginard@xtec.cat.
+        if (!str_contains($username, '@xtec.cat')) {
+            // Username is correct, so the email must be created by adding the domain.
+            $email = $username . '@xtec.cat';
+        } else {
+            // Username contains the email, so the domain must be removed.
+            $email = $username;
+            $username = str_replace('@xtec.cat', '', $username);
+        }
+
+        // Look for the user in the database.
+        $user = User::where('email', $email)->first();
+
+        // If the user does not exist, create it.
+        if (is_null($user)) {
+            $user = new User([
+                'name' => $username,
+                'email' => $email,
+                'password' => '',
+            ]);
+            $user->save();
+        }
+
+        $current_client = Cache::get_current_client($request);
+
+        if (Manager::where('user_id', $user->id)->where('client_id', $current_client['id'])->exists()) {
+            return redirect()->back()->withErrors(__('manager.manager_already_exists'));
+        }
+
+        $manager = new Manager([
+            'user_id' => $user->id,
+            'client_id' => $current_client['id'],
+        ]);
+        $manager->save();
+
+        return redirect()->back()->with('success', __('manager.manager_added'));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Manager $manager)
-    {
+    public function show(Manager $manager) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Manager $manager)
-    {
+    public function edit(Manager $manager) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateManagerRequest $request, Manager $manager)
-    {
+    public function update(UpdateManagerRequest $request, Manager $manager) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Manager $manager)
-    {
-        //
+    public function destroy(Manager $manager) {
+        $manager->delete();
+        return redirect()->back()->with('success', __('manager.manager_removed'));
     }
 }
---
+++ lang/ca/manager.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'manager' => 'Gestor',
+    'managers' => 'Gestors',
+    'manager_list' => 'Gestors del centre :name',
+    'no_managers' => 'No s\'ha designat cap gestor',
+    'add_manager' => 'Afegeix un gestor',
+    'add_manager_text' => 'Nom d\'usuari o adreça XTEC del nou gestor',
+    'notify_user' => 'Aviseu a la persona designada que a partir d\'ara podrà gestionar els serveis del centre des d\'aquest portal.',
+    'manager_added' => 'S\'ha afegit el gestor.',
+    'manager_removed' => 'S\'ha esborrat el gestor.',
+    'manager_already_exists' => 'Aquest gestor ja estava assignat.',
+    'max_managers' => 'El nombre màxim permès de gestors és :number. No es poden afegir més gestors.',
+    'username_required' => 'Cal que indiqueu el nom d\'usuari o l\'adreça XTEC del nou gestor.',
+    'username_string' => 'El text introduït ha de ser una cadena de caràcters.',
+    'username_max' => 'El text introduït no pot tenir més de 17 caràcters.',
+    'username_regex' => 'El text introduït no es correspon amb un nom d\'usuari XTEC ni amb una adreça de correu XTEC.',
+];
