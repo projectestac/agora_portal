ed19497a8684f67c8308ddcab23959c346a1a855
Toni Ginard <toni.ginard@gmail.com>
2023-06-02 11:40:11 +0200

    Service: Synced changes and minor improvements

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -18,34 +18,20 @@ class Service extends Model {
         'quota',
     ];
 
-    public function clients($orderBy = 'id', $direction = 'asc'): BelongsToMany {
-        return $this->belongsToMany(Client::class)
-            ->withPivot([
-                'id',
-                'status',
-                'db_id',
-                'db_host',
-                'quota',
-                'used_quota',
-                'visible',
-                'model_type_id',
-                'contact_name',
-                'contact_profile',
-                'observations',
-                'annotations',
-                'requested_at',
-                'created_at',
-                'updated_at',
-            ])
-            ->orderBy($orderBy, $direction);
-    }
-
     public function requests(): HasMany {
         return $this->hasMany(Request::class);
     }
 
     public function commands(): HasMany {
         return $this->hasMany(Command::class);
+    }
+
+    public function instances(): HasMany {
+        return $this->hasMany(Instance::class);
+    }
+
+    public function modelTypes(): HasMany {
+        return $this->hasMany(ModelType::class);
     }
 
     public function requestTypes(): BelongsToMany {
