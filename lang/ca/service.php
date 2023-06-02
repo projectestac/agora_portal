586d6bdb68f6661afeb8e9ce14e4c156543d111e
Toni Ginard <toni.ginard@gmail.com>
2023-06-02 11:40:11 +0200

    Service: Synced changes and minor improvements

--- lang/ca/service.php
+++ lang/ca/service.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'name' => 'Nom',
+    'name' => 'Nom del servei',
     'description' => 'Descripció',
     'url' => 'URL',
     'quota' => 'Quota',
     'actions' => 'Accions',
-    'show' => 'Mostra',
-    'edit' => 'Edita',
-    'delete' => 'Esborra',
+    'show' => 'Mostra el servei',
+    'edit' => 'Edita el servei',
+    'delete' => 'Esborra el servei',
     'service' => 'Servei',
     'services' => 'Serveis',
     'service_list' => 'Llista de serveis',
