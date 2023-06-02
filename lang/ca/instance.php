15abf671d9c3b444ed4f45dd0a4f60618d32414b
Toni Ginard <toni.ginard@gmail.com>
2023-06-02 11:34:12 +0200

    Instance: Moved from pivot table to model and continued development

--- lang/ca/instance.php
+++ lang/ca/instance.php
@@ -4,4 +4,5 @@ return [
     'instance_list' => 'Llista d\'instàncies',
     'db_id' => 'Base de dades',
     'no_instances' => 'No hi ha cap instància',
+    'location_long' => 'Municipi i ST',
 ];
--- resources/views/myagora/instance.blade.php
+++ resources/views/myagora/instance.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.default')
 
 @php
-    use Carbon\Carbon;
     use App\Helpers\Util;
+    use Carbon\Carbon;
 @endphp
 
 @section('content')
@@ -11,8 +11,8 @@
     </div>
 
     <div class="content myagora">
-        @if (isset($current_client['name']))
-            <h3>{{ __('myagora.instance_list', ['name' => $current_client['name']]) }}</h3>
+        @if (isset($currentClient['name']))
+            <h3>{{ __('myagora.instance_list', ['name' => $currentClient['name']]) }}</h3>
         @endif
 
         @include('components.messages')
@@ -20,21 +20,21 @@
         @if (!empty($instances))
             @foreach($instances as $instance)
                 @php
-                    $date = Carbon::parse($instance->pivot->created_at);
-                    $quota = Util::formatBytes($instance->pivot->quota);
-                    $usedQuota = Util::formatBytes($instance->pivot->used_quota);
+                    $date = Carbon::parse($instance->created_at);
+                    $quota = Util::formatBytes($instance->quota);
+                    $usedQuota = Util::formatBytes($instance->used_quota);
                 @endphp
 
                 <div class="panel panel-default">
                     <div class="panel-heading row-fluid clearfix">
-                        {{ $instance->name }}: {{ $instance->description }}
+                        {{ $instance->service->name }}: {{ $instance->service->description }}
                     </div>
                     <div class="panel-body">
                         <ul>
-                            <li><strong>{{ __('myagora.requested_by') }}:</strong> {{ $instance->pivot->contact_name }}</li>
+                            <li><strong>{{ __('myagora.requested_by') }}:</strong> {{ $instance->contact_name }}</li>
                             <li><strong>{{ __('myagora.active_date') }}:</strong> {{ $date->format('d/m/Y') }}</li>
-                            <li><strong>{{ __('common.status') }}:</strong> {{ $instance->pivot->status }}</li>
-                            <li><strong>{{ __('myagora.database') }}:</strong> {{ $instance->pivot->db_id }}</li>
+                            <li><strong>{{ __('common.status') }}:</strong> {{ $instance->status }}</li>
+                            <li><strong>{{ __('myagora.database') }}:</strong> {{ $instance->db_id }}</li>
                             <li><strong>{{ __('service.quota') }}:</strong> ({{ $usedQuota }} / {{ $quota }})</li>
                         </ul>
                     </div>
@@ -42,6 +42,28 @@
             @endforeach
         @else
             <div class="alert alert-warning">{{ __('myagora.no_instances') }}</div>
+        @endif
+
+        @if (!empty($availableServices))
+            <div class="panel panel-default">
+                <div class="panel-heading row-fluid clearfix">
+                    {{ __('myagora.other_services_available') }}
+                </div>
+                <div class="panel-body">
+                    @foreach($availableServices as $service)
+                        <div style="margin: 10px 0 10px 0">
+                            <a class="btn btn-default" href="{{ route('instances.create', ['service_id' => $service['id']]) }}" role="button">
+                                <img src="{{ asset('images/' . mb_strtolower($service['name']) . '.gif') }}"
+                                     alt="{{ $service['name'] }}"
+                                     title="{{ $service['name'] }}"
+                                >
+                                <span>{{ __('myagora.request_now') }}</span>
+                            </a>
+                            <strong>{{ $service['name'] }}</strong>: {{ $service['description'] }}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         @endif
 
     </div>
