6940241c9cbc37bde44dbaa31cf5464d82ca9af5
Toni Ginard <toni.ginard@gmail.com>
2023-06-07 11:41:08 +0200

    Helpers: Added functions for upload large files functionality

--- app/Helpers/Cache.php
+++ app/Helpers/Cache.php
@@ -3,13 +3,17 @@
 namespace App\Helpers;
 
 use App\Models\Client;
+use App\Models\Instance;
+use App\Models\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 /**
  * The Laravel session will be used as a cache to store several data.
  */
 class Cache {
+
     public static function getClients(Request $request): mixed {
         $clients = $request->session()->get('clients');
 
@@ -67,4 +71,79 @@ class Cache {
 
         return $currentClient;
     }
+
+    public static function getCurrentInstance(Request $request, $clientId = 0, $serviceName = 'Moodle') {
+
+        $currentInstance = $request->session()->get('currentInstance');
+
+        if (!is_null($currentInstance)) {
+            return $currentInstance;
+        }
+
+        if (empty($clientId)) {
+            $currentClient = self::getCurrentClient($request);
+            $clientId = $currentClient['id'];
+        }
+
+        $serviceId = Service::where('name', $serviceName)
+            ->pluck('id')
+            ->first();
+
+        $currentInstance = Instance::where('client_id', $clientId)
+            ->where('service_id', $serviceId)
+            ->where('status', 'active')
+            ->orderBy('id', 'desc')
+            ->first();
+
+        // Protect against the case where there is no instance for the specified client and service.
+        if ($currentInstance) {
+            $currentInstanceArray = $currentInstance->toArray();
+        }
+
+        $request->session()->put('currentInstance', $currentInstanceArray);
+
+        return $currentInstanceArray;
+
+    }
+
+    /**
+     * Get the database name for the specified service. It checks if it was already stored in the session. If not,
+     * it will be calculated and stored in the session. In that case, the client used to calculate the database name
+     * would be the current client (the one stored in the session).
+     *
+     * @param Request $request
+     * @param string $serviceName
+     * @return string
+     */
+    public static function getDBName(Request $request, string $serviceName = 'Moodle'): string {
+
+        $dbName = $request->session()->get('dbName_' . $serviceName);
+
+        if (!is_null($dbName)) {
+            return $dbName;
+        }
+
+        if (is_null($request)) {
+            return '';
+        }
+
+        $currentClient = self::getCurrentClient($request);
+
+        $serviceId = Service::where('name', $serviceName)
+            ->where('status', 'active')
+            ->pluck('id')
+            ->first();
+
+        $instanceId = Instance::where('client_id', $currentClient['id'])
+            ->where('service_id', $serviceId)
+            ->where('status', 'active')
+            ->pluck('id')
+            ->first();
+
+        $dbName = Config::get('app.agora.moodle2.userprefix') . $instanceId;
+        $request->session()->put('dbName_' . $serviceName, $dbName);
+
+        return $dbName;
+    }
+
 }
