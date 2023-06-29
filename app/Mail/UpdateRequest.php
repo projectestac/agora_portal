9522e9dfcfc44898631e198dd33ff98cc18465a2
Toni Ginard <toni.ginard@gmail.com>
2023-06-29 13:53:36 +0200

    Email: Added classes for update instance and request

---
+++ app/Mail/UpdateRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateRequest extends Mailable {
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance. Constructor uses "Constructor Property Promotion" (PHP 8).
+     */
+    public function __construct(private $status, private $adminComments) {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope {
+        return new Envelope(
+            subject: __('mail.update_request_subject'),
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content {
+        return new Content(
+            view: 'mail.update-request',
+            with: [
+                'status' => $this->status,
+                'adminComments' => $this->adminComments,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array {
+        return [];
+    }
+}
