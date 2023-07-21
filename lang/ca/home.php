478ce22fe5358c7f89c6e9f029f7a923ffb6a17e
Toni Ginard <toni.ginard@gmail.com>
2023-07-21 17:17:37 +0200

    Home page: Added disclaimer

--- lang/ca/home.php
+++ lang/ca/home.php
@@ -4,4 +4,6 @@ return [
     'title' => 'Portal Àgora',
     'department' => 'Departament d\'Educació',
     'xtec' => 'Xarxa Telemàtica Educativa de Catalunya',
+    'active_instances' => 'Llista d\'espais actius',
+    'temporarily_unavailable' => 'Temporalment no disponible',
 ];
