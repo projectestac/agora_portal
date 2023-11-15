b61d7bc2a82dcc23a9fed566296208cb80420070
Toni Ginard <toni.ginard@gmail.com>
2023-11-15 14:53:08 +0100

    Util Access: Allow class Authenticatable for user objects

--- app/Helpers/Access.php
+++ app/Helpers/Access.php
@@ -3,17 +3,22 @@
 namespace App\Helpers;
 
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class Access {
-    public static function isAdmin(User $user): bool {
+    public static function isAdmin(User|Authenticatable $user): bool {
         return $user->hasPermissionTo('Administrate site');
     }
 
-    public static function isClient(User $user): bool {
+    public static function isClient(User|Authenticatable $user): bool {
         return $user->hasPermissionTo('Manage own managers');
     }
 
-    public static function isManager(User $user): bool {
+    public static function isManager(User|Authenticatable $user): bool {
         return $user->hasPermissionTo('Manage clients');
+    }
+
+    public static function isUser(User|Authenticatable $user): bool {
+        return !(self::isManager($user) || self::isClient($user) || self::isAdmin($user));
     }
 }
