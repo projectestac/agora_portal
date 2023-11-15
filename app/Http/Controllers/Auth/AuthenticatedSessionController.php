4b8fe1e3064329c7701c71fae7405b606b1e06f8
Toni Ginard <toni.ginard@gmail.com>
2023-11-15 14:54:11 +0100

    Login: Automatically add users to role 'user' on login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -10,7 +10,11 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
 
+/**
+ * Controller used when the user logs in with local credentials. For Google login, see GoogleController.
+ */
 class AuthenticatedSessionController extends Controller {
     /**
      * Display the login view.
@@ -27,11 +31,15 @@ class AuthenticatedSessionController extends Controller {
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse {
+
         $request->authenticate();
 
         $request->session()->regenerate();
 
         $user = Auth::user();
+        if (is_null($user)) {
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
 
         if (is_object($user)) {
             $user->last_login_at = now();
@@ -46,7 +54,17 @@ class AuthenticatedSessionController extends Controller {
             return redirect()->intended(RouteServiceProvider::MY_AGORA);
         }
 
+        // If user has logged in and is not an admin, a client or a manager, it must have the role User.
+        if (Access::isUser($user)) {
+            // Check if user has role User.
+            $userRole = Role::findByName('user');
+            if (!$user->hasRole($userRole)) {
+                $user->assignRole($userRole);
+            }
+        }
+
         return redirect()->intended(RouteServiceProvider::HOME);
+
     }
 
     /**
