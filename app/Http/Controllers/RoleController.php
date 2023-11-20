3511018e3cf9634795b8e3850b7fc42154bdc851
Toni Ginard <toni.ginard@gmail.com>
2023-11-20 14:33:15 +0100

    Role: Improvements in list of roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,62 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Role;
+use Illuminate\View\View;
 
-class RoleController extends Controller
-{
+class RoleController extends Controller {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index(): View {
+        $roles = \Spatie\Permission\Models\Role::paginate(25);
+        return view('admin.role.index')->with('roles', $roles);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create() {
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Role $role)
-    {
-        //
+    public function show(Role $role) {
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Role $role)
-    {
-        //
+    public function edit(Role $role) {
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, string $id) {
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Role $role)
-    {
-        //
+    public function destroy(Role $role) {
     }
 }
