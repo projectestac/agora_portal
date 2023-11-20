304c7675c2005028cb8e3c18b162674782b73f3c
Corentin Robin <corentin.robin@ticxcat.cat>
2023-11-20 12:13:00 +0100

    ModelType: Implemented List, Creation, Edition and Update

--- app/Http/Controllers/ModelTypeController.php
+++ app/Http/Controllers/ModelTypeController.php
@@ -5,31 +5,56 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreModelTypeRequest;
 use App\Http\Requests\UpdateModelTypeRequest;
 use App\Models\ModelType;
-use Illuminate\Contracts\Foundation\Application as ApplicationContract;
-use Illuminate\Contracts\View\Factory;
+use App\Models\Service;
 use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 
 class ModelTypeController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index(): View|Application|Factory|ApplicationContract  {
-        return view('admin.model.index');
+    public function index(): View {
+        $modelTypes = ModelType::with('service')->get();
+
+        return view('admin.model-type.index')
+            ->with('modelTypes', $modelTypes);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {
-        //
+    public function create(): View {
+        $services = Service::all()->sortBy('name');
+
+        return view('admin.model-type.create')
+            ->with('services', $services);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreModelTypeRequest $request) {
-        //
+    public function store(StoreModelTypeRequest $request): RedirectResponse {
+
+        $modelType = new ModelType([
+            'service_id' => $request->input('service_id'),
+            'description' => $request->input('description'),
+            'short_code' => $request->input('short_code'),
+            'url' => $request->input('url'),
+            'db' => $request->input('db'),
+        ]);
+
+        try {
+            $modelType->save();
+        } catch (\Exception $e) {
+            return redirect()
+                ->route('model-types.create')
+                ->withErrors(['error' => $e->getMessage()]);
+        }
+
+        return redirect()
+            ->route('model-types.index')
+            ->with('success', __('modeltype.created_model_short'));
+
     }
 
     /**
@@ -42,21 +67,43 @@ class ModelTypeController extends Controller {
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(ModelType $modelType) {
-        //
+    public function edit($id): View {
+        $services = Service::all()->sortBy('name');
+        $modelType = ModelType::findOrFail($id);
+
+        return view('admin.model-type.edit')
+            ->with('modelType', $modelType)
+            ->with('services', $services);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateModelTypeRequest $request, ModelType $modelType) {
-        //
+    public function update(UpdateModelTypeRequest $request, $id): RedirectResponse {
+        $modelType = ModelType::findOrFail($id);
+
+        $modelType->update([
+            'service_id' => $request->input('service_id'),
+            'description' => $request->input('description'),
+            'short_code' => $request->input('short_code'),
+            'url' => $request->input('url'),
+            'db' => $request->input('db'),
+        ]);
+
+        return redirect()
+            ->route('model-types.index')
+            ->with('success', __('request.request_created'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ModelType $modelType) {
-        //
+    public function destroy($id): RedirectResponse {
+        $modelType = ModelType::findOrFail($id);
+        $modelType->delete();
+
+        return redirect()
+            ->route('model-types.index')
+            ->with('success', __('common.deletion_success'));
     }
 }
---
+++ lang/ca/modeltype.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'model' => 'Maqueta',
+    'models' => 'Maquetes',
+    'model_edit' => 'Edita la maqueta',
+    'new_model' => 'Maqueta nova',
+    'request_created' => 'S\'ha registrat la vostra maqueta',
+    'no_models' => 'No hi ha maquetes',
+    'select_model' => 'Indiqueu la plantilla que voleu fer servir',
+    'description_required' => 'Cal que indiqueu una descripció',
+    'description_string' => 'La descripció ha de ser una cadena de text',
+    'description_max' => 'La descripció no pot tenir més de 255 caràcters',
+    'short_code_required' => 'Cal que indiqueu un codi curt',
+    'short_code_string' => 'El codi curt ha de ser una cadena de text',
+    'short_code_max' => 'El codi curt no pot tenir més de 255 caràcters',
+    'url_url' => 'El camp URL ha de ser un URL vàlid',
+    'url_max' => 'L\'URL no pot tenir més de 255 caràcters',
+    'db_string' => 'La base de dades ha de ser una cadena de text',
+    'db_max' => 'La base de dades no pot tenir més de 255 caràcters',
+];
