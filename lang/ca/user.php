d0dcd5f25eb4078b1843fd1fb7bf610d729f6734
Toni Ginard <toni.ginard@gmail.com>
2023-11-20 14:33:54 +0100

    User: Improvements in list of users

--- lang/ca/user.php
+++ lang/ca/user.php
@@ -3,6 +3,10 @@
 return [
     'users' => 'Usuaris',
     'user' => 'Usuari',
+    'users_list' => 'Llista d\'usuaris',
     'name' => 'Nom d\'usuari',
     'email' => 'Correu electrònic',
+    'roles' => 'Rols',
+    'roles_list' => 'Llista de rols',
+    'guard' => 'Accés',
 ];
