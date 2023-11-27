68954509dacad1786bf678ff7b7e61c0baa23a5c
Toni Ginard <toni.ginard@gmail.com>
2023-11-27 13:19:46 +0100

    Fixed and improved change of DNS

--- app/Http/Requests/StoreInstanceRequest.php
+++ app/Http/Requests/StoreInstanceRequest.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Requests;
 
+use App\Helpers\Access;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreInstanceRequest extends FormRequest
-{
+class StoreInstanceRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
+    public function authorize(): bool {
+        return Access::isAdmin($this->user()) || Access::isManager($this->user());
     }
 
     /**
@@ -19,10 +18,22 @@ class StoreInstanceRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
-            //
+            'client_id' => 'integer|required',
+            'service_id' => 'integer|required',
+            'model_type_id' => 'integer|required',
+            'contact_profile' => 'string|required',
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'client_id.required' => __('instance.client_id_required'),
+            'service_id.required' => __('instance.service_id_required'),
+            'model_type_id.required' => __('instance.model_type_id_required'),
+            'contact_profile.string' => __('instance.contact_profile_required'),
+            'contact_profile.required' => __('instance.contact_profile_required'),
         ];
     }
 }
