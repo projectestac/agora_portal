c7a15449b183a4bb447d821ee1d23847a898cf4f
Alejandro Escobar <alejandroescobar.98@gmail.com>
2024-02-27 12:08:14 +0100

    Users: Add edit page

--- resources/views/admin/user/action.blade.php
+++ resources/views/admin/user/action.blade.php
@@ -1,6 +1,6 @@
-<a href="{{ route('users.show', $user->id) }}" class="btn btn-info" title="{{ __('common.show') }}">
+{{-- <a href="{{ route('users.show', $user->id) }}" class="btn btn-info" title="{{ __('common.show') }}">
     <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-</a>
+</a> --}}
 <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
     <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 </a>
