8ed8b089969ec0de5eb3546c639be50b67f63842
J. Alejandro Escobar M <alejandroescobar.98@gmail.com>
2024-04-03 09:10:54 +0200

    Users list: Managers have extra action

--- resources/views/admin/user/action.blade.php
+++ resources/views/admin/user/action.blade.php
@@ -1,6 +1,12 @@
 {{-- <a href="{{ route('users.show', $user->id) }}" class="btn btn-info" title="{{ __('common.show') }}">
     <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
 </a> --}}
+{{-- {{dump($user->manager)}} --}}
+@if ($user->hasRole('manager'))
+    <a href="{{ route('manager.wipFunction', $user) }}" class="btn btn-warning" title="{{ __('manager.manager') }}">
+        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+    </a>
+@endif
 <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
     <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 </a>
