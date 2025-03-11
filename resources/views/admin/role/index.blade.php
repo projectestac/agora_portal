62497ea4c417eb52a2ea097a67b70443184fe245
Corentin Robin <corentin.robin@ticxcat.cat>
2025-03-11 13:35:23 +0100

    Añadido formulario para crear roles, lógica de almacenamiento y confirmación de eliminación. Mejoras en la interfaz y traducciones actualizadas.

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -7,6 +7,15 @@
 
     <div class="content users">
         <h3>{{ __('user.roles_list') }}</h3>
+
+        <div class="row">
+            <div class="col-md-6">
+                <a href="{{ route('roles.create') }}" class="btn btn-primary">{{ __('role.add_role') }}</a>
+            </div>
+        </div>
+
+        <br>
+
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -26,6 +35,15 @@
                             <a href="{{ route('role.edit', $role->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
                                 <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                             </a>
+                        </td>
+                        <td>
+                            <form action="{{ route('role.destroy', $role->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('{{ __('role.confirm_delete') }}');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" title="{{ __('common.delete') }}">
+                                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                </button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
