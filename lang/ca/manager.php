cfa22401f6caeae4f66cc9da5f4b65526f9a5ae9
Corentin Robin <corentin.robin@ticxcat.cat>
2025-03-11 10:57:27 +0100

    Añadido formulario para crear nuevos gestores y lógica para almacenar gestores en la base de datos. Mejoras en la interfaz de usuario y traducciones actualizadas.

--- lang/ca/manager.php
+++ lang/ca/manager.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    'id' => 'ID',
     'manager' => 'Gestor',
     'managers' => 'Gestors',
-    'manager_list' => 'Gestors del centre :name',
+    'manager_list' => 'Llista de gestors',
+    'new_manager' => 'Nou gestor',
     'no_managers' => 'No s\'ha designat cap gestor',
     'add_manager' => 'Afegeix un gestor',
     'add_manager_text' => 'Nom d\'usuari o adreça XTEC del nou gestor',
@@ -18,11 +20,14 @@ return [
     'edit_manager' => 'Edita el gestor',
     'client' => 'Client',
     'user' => 'Usuari',
+    'select_user' => 'Selecciona un usuari',
+    'select_client' => 'Selecciona un client',
     'max_managers' => 'El nombre màxim permès de gestors és :number. No es poden afegir més gestors.',
     'username_required' => 'Cal que indiqueu el nom d\'usuari o l\'adreça XTEC del nou gestor.',
     'username_string' => 'El text introduït ha de ser una cadena de caràcters.',
     'username_max' => 'El text introduït no pot tenir més de 17 caràcters.',
     'username_regex' => 'El text introduït no es correspon amb un nom d\'usuari XTEC ni amb una adreça de correu XTEC.',
     'clients_cannot_be_managers' => 'Els codis de centre no poden ser gestors.',
+    'client_id_required' => 'Cal que indiqueu el client.',
     'remove_manager_confirm' => 'Confirmeu que voleu que l\'usuari deixi de ser gestor/a?',
 ];
