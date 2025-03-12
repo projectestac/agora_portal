bfb0298d1434688da4b7af303f4926018bbb5df6
Corentin Robin <corentin.robin@ticxcat.cat>
2025-03-12 15:36:24 +0100

    Añadido formulario para crear usuarios con validación y almacenamiento.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Models\Role;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\HtmlString;
@@ -26,12 +27,26 @@ class UserController extends Controller {
      * Show the form for creating a new resource.
      */
     public function create() {
+        return view('admin.user.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        return redirect()->route('users.index')->with('success', __('user.user_created'));
     }
 
     /**
