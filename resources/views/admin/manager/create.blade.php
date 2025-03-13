78af1c9bd2ab59fa64d8804a9b059eb8238c758c
Corentin Robin <corentin.robin@ticxcat.cat>
2025-03-13 15:38:55 +0100

    Actualizada la interfaz de creación de gestores para usar campos de texto con autocompletado en lugar de listas desplegables.

--- resources/views/admin/manager/create.blade.php
+++ resources/views/admin/manager/create.blade.php
@@ -10,34 +10,25 @@
 
         @include('components.messages')
 
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/smoothness/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
         <form class="form-horizontal" action="{{ route('managers.store_new') }}" method="POST">
             @csrf
 
             <div class="form-group">
                 <label class="col-sm-4 control-label clear" for="username">{{ __('manager.user') }}</label>
                 <div class="col-sm-8">
-                    <select name="username" id="username" class="form-control">
-                        <option value="" selected disabled>{{ __('manager.select_user') }}</option>
-                        @foreach($users as $user)
-                            <option value="{{ $user->name }}">
-                                {{ $user->name }}
-                            </option>
-                        @endforeach
-                    </select>
+                    <input type="text" id="username" class="form-control" placeholder="{{ __('manager.type_user') }}">
+                    <input type="hidden" name="username" id="username_hidden">
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-sm-4 control-label clear" for="client_id">{{ __('manager.client') }}</label>
                 <div class="col-sm-8">
-                    <select name="client_id" id="client_id" class="form-control">
-                        <option value="" selected disabled>{{ __('manager.select_client') }}</option>
-                        @foreach($clients as $client)
-                            <option value="{{ $client->id }}">
-                                {{ $client->name }}
-                            </option>
-                        @endforeach
-                    </select>
+                    <input type="text" id="client_id" class="form-control" placeholder="{{ __('manager.type_client') }}">
+                    <input type="hidden" name="client_id" id="client_id_hidden">
                 </div>
             </div>
 
@@ -48,6 +39,36 @@
             </div>
 
         </form>
+
+        <script>
+            $(document).ready(function () {
+                var users = @json($users->pluck('name'));
+                var clients = @json($clients->pluck('name', 'id'));
+
+                $("#username").autocomplete({
+                    source: users,
+                    select: function (event, ui) {
+                        $("#username_hidden").val(ui.item.value);
+                    }
+                });
+
+                $("#client_id").autocomplete({
+                    source: Object.values(clients),
+                    select: function (event, ui) {
+                        var selectedClientId = Object.keys(clients).find(key => clients[key] === ui.item.value);
+                        $("#client_id_hidden").val(selectedClientId);
+                    }
+                });
+            });
+        </script>
+
+        <style>
+            .ui-autocomplete {
+                max-height: 400px;
+                overflow-y: auto;
+                overflow-x: hidden;
+            }
+        </style>
     </div>
 
 @endsection
