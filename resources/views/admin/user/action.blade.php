2798816aa7077812ddcf48cd165f4e788144ba3f
Corentin Robin <corentin.robin@ticxcat.cat>
2025-03-13 10:15:19 +0100

    Añadido componente de modal de confirmación de eliminación para usuarios y roles. Se actualizó la lógica de eliminación en los controladores.

--- resources/views/admin/user/action.blade.php
+++ resources/views/admin/user/action.blade.php
@@ -1,13 +1,9 @@
-{{-- <a href="{{ route('users.show', $user->id) }}" class="btn btn-info" title="{{ __('common.show') }}">
-    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-</a> --}}
 <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
     <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 </a>
-<form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-danger" title="{{ __('common.delete') }}">
-        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-    </button>
-</form>
+
+@include('components.confirm-delete-modal', [
+    'id' => $user->id,
+    'name' => $user->name,
+    'route' => route('users.destroy', $user->id)
+])
