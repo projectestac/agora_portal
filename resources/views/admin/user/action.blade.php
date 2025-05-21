3f2e342381403be861005e316ebcfd726a53ba91
Corentin Robin <corentin.robin@ticxcat.cat>
2025-05-21 14:31:25 +0200

    Users and Roles: Added restore functionality

--- resources/views/admin/user/action.blade.php
+++ resources/views/admin/user/action.blade.php
@@ -1,15 +1,20 @@
 <div style="text-align: right; margin-right: 20px;">
 
-    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
-        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-    </a>
-
-    {{-- Users already soft-deleted don't show delete button --}}
+    {{-- Users already soft-deleted don't show delete or edit button --}}
     @if(!$user->deleted_at)
+        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary" title="{{ __('common.edit') }}">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+        </a>
+
         @include('components.confirm-delete-modal', [
             'id' => $user->id,
             'name' => $user->name,
             'route' => route('users.destroy', $user->id)
+        ])
+    @else
+        @include('components.confirm-restore-modal', [
+            'id' => $user->id,
+            'restoreRoute' => route('users.restore', $user->id)
         ])
     @endif
 
