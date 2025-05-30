ec34d3eba794f3b0bec792215ee1e380987cfa78
Corentin Robin <corentin.robin@ticxcat.cat>
2025-05-30 14:01:51 +0200

    Added application log for all HTTP requests for audit. Includes command to remove old records and middleware to register access.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('clean:stats-exports')->daily();
+        $schedule->command('logs:purge-old')->daily();
     }
 
     /**
