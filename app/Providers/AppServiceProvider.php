b79addc3bbdd9e5edce0b64be595c453a878feec
Corentin Robin <corentin.robin@ticxcat.cat>
2025-06-03 15:56:50 +0200

    Clients: Implemented client switching functionality and updated menu view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+use App\Models\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('menu.clientmenu', static function ($view) {
+            $user = Auth::user();
+
+            if ($user) {
+                $clients = Client::whereIn('id', static function ($query) use ($user) {
+                    $query->select('client_id')
+                        ->from('managers')
+                        ->where('user_id', $user->id);
+                })->get();
+
+                $currentClient = session('currentClient');
+
+                $view->with(compact('clients', 'currentClient'));
+            }
+        });
     }
 }
