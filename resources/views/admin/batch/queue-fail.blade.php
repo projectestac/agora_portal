72723c8335f4856183e8dae1af53bc375bf85ef2
Corentin Robin <corentin.robin@ticxcat.cat>
2025-08-20 12:13:04 +0200

    Added support for searching in successful and failed jobs. A reusable search component was created, and pagination was improved with query parameters.

--- resources/views/admin/batch/queue-fail.blade.php
+++ resources/views/admin/batch/queue-fail.blade.php
@@ -25,7 +25,11 @@
 
             <div id="tab3" class="tab-pane fade in active">
                 @if(is_array($data))
+
                     <br>
+
+                    <x-batch-search :action="url()->current()" />
+
                     <div class="pull-right">
                         {!! $links !!}
                     </div>
