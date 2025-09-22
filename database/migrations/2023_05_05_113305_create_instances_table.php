88ebc365a9c1514c21657e6e73ac51132e7ca18e
Toni Ginard <toni.ginard@gmail.com>
2025-09-22 13:39:03 +0200

    Migration: Fixed rollback function

--- database/migrations/2023_05_05_113305_create_instances_table.php
+++ database/migrations/2023_05_05_113305_create_instances_table.php
@@ -45,6 +45,6 @@ return new class extends Migration {
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::dropIfExists('client_services');
+        Schema::dropIfExists('instances');
     }
 };
